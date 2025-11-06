94af4946db7f2f87abb5e4b104fbef2e25a65dff
edu-niknei <nikolas.neimanis@skola.taby.se>
2025-11-06 12:28:02 +0100

    Create quiz.py
    En fråga med svasalternativ

---
+++ quiz.py
@@ -0,0 +1,7 @@
+
+svar = int(input("vilken huvudstad har sverige?\n1. Oslo\n2. Stockholm\n3. Köpenhamn\n"))
+if svar == 2:
+    print(f"ditt val: {svar}")
+    print("Rätt!")
+else:
+    print("Fel rätt svar var: Stockholm")
